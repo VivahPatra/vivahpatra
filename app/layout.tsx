74c3f9b2af7c567a6ad7d4913c7051556f36c1c9
spwr97 <spwr97@gmail.com>
2026-07-20 08:38:40 +0530

    feat: add Meta Pixel 1702814747452964

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,6 +128,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1702814747452964');
+          fbq('track', 'PageView');
+        `}</Script>
+        <noscript><img height="1" width="1" style={{display:'none'}}
+          src="https://www.facebook.com/tr?id=1702814747452964&ev=PageView&noscript=1"
+        /></noscript>
         {process.env.NEXT_PUBLIC_GA_ID && (
           <>
             <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} strategy="afterInteractive" />
